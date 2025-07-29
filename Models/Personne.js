f2b73df3107db75f55f86b956da280ae22d0d955
Votre Nom <mohammedsalhisam@gmail.com>
2025-07-29 16:02:32 +0100

    models

---
+++ Models/Personne.js
@@ -0,0 +1,12 @@
+const PersonneSchema = new mongoose.Schema({
+
+  type: { type: String, enum: ["physique", "morale"], required: true },
+  nom: String,       // pour physique
+  prenom: String,    // pour physique
+  denomination: String, // pour morale
+  email: { type: String, required: true, unique: true },
+  telephone: String,
+    
+});
+
+const Personne = mongoose.model("Personne", PersonneSchema);
