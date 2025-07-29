609bc2f7923d4d2b975be5b52ebec21291580bed
ahyahya1616 <ahmaneyahya10@gmail.com>
2025-07-29 14:27:52 +0100

    ajout de fichier server.js , .env et db.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("./config/db"); // Import de la fonction de connexion
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connexion à MongoDB
+connectDB();
+
+// Lancement du serveur
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Serveur lancé sur le port ${PORT}`);
+});
